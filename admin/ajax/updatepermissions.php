c6fc85cd3cc23fc867a86a029cc220f701383573
HabboPHP <habbophp.com@gmail.com>
2012-12-03 20:51:00 +0100

    Add security permissions

--- admin/ajax/updatepermissions.php
+++ admin/ajax/updatepermissions.php
@@ -3,9 +3,9 @@
 require '../includes/init.php';
 if(!Tools::checkACL($user->rank,ACL_USERS_PERMISSIONS)) { exit ; }
 
-if(isset($_GET['id']) && isset($_GET['c'])){
+if(isset($_GET['id']) && isset($_GET['c']) && in_array(array('1','0'),$_GET['c'])){
 	$rank = substr($_GET['id'],-1);
 	$cmd =  substr($_GET['id'],0,-2);
-	mysql_query('UPDATE permissions_ranks SET '.$cmd.'="'.$_GET['c'].'" WHERE rank='.intval($rank));
+	mysql_query('UPDATE permissions_ranks SET '.$cmd.'="'.intval($_GET['c']).'" WHERE rank='.intval($rank));
 	if(addLog($user->username,"Update permissions ".$cmd." rank : ".$rank."")) { echo "1"; }
 }
