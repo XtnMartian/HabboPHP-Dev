6219712608d46a01bbbea826fc34bc3842ed4daa
HabboPHP <habbophp.com@gmail.com>
2012-12-23 11:37:24 +0100

    XML register

--- admin/ajax/updatepermissions.php
+++ admin/ajax/updatepermissions.php
@@ -3,7 +3,7 @@
 require '../includes/init.php';
 if(!Tools::checkACL($user->rank,ACL_USERS_PERMISSIONS)) { exit ; }
 
-if(isset($_GET['id']) && isset($_GET['c']) && in_array($_GET['c'],array('1','0')){
+if(isset($_GET['id']) && isset($_GET['c']) && in_array($_GET['c'],array('1','0'))){
 	$rank = substr($_GET['id'],-1);
 	$cmd =  substr($_GET['id'],0,-2);
 	mysql_query('UPDATE permissions_ranks SET '.$cmd.'="'.intval($_GET['c']).'" WHERE rank='.intval($rank));
